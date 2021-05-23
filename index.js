9cb674381f7924b1cdb75be739b0246d9cc5b44d
elliotyou <m9402311@mail.ntust.edu.tw>
2021-05-23 19:04:44 +0800

    make all 52 cards appear

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const view = {
   },
   displayCars() {
     const rootElement = document.querySelector('#cards')
-    rootElement.innerHTML = this.getCardElement(10)
+    rootElement.innerHTML = Array.from(Array(52).keys()).map(index => this.getCardElement(index)).join("")
   },
   transformNumber(number) {
     switch (number) {

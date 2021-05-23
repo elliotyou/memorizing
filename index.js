26e30bd06c87214a76d81b854227607514dc64de
elliotyou <m9402311@mail.ntust.edu.tw>
2021-05-23 18:46:56 +0800

    seperate into 2 functions

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const view = {
-  displayCars() {
-    document.querySelector('#cards').innerHTML = `
+  getCardElement() {
+    return `
       <div class="card">
         <p>4</p>
         <img src="https://image.flaticon.com/icons/svg/105/105220.svg">
         <p>4</p>
-      </div>
-    `
+      </div>`
+  },
+  displayCars() {
+    const rootElement = document.querySelector('#cards')
+    rootElement.innerHTML = this.getCardElement()
   }
 }
 
-view.displayCars()
+view.displayCars() 

1c347a32983b84d1b5182897ca8693ec32ec0152
elliotyou <m9402311@mail.ntust.edu.tw>
2021-05-23 19:14:12 +0800

    design a tool to shuffle an number array

--- index.js
+++ index.js
@@ -37,4 +37,17 @@ const view = {
   }
 }
 
-view.displayCars() 
+const utility = {
+  getRandomNumberArray(count) {
+    const number = Array.from(Array(count).keys())
+    for (let index = number.length - 1; index > 0; index--) {
+      let randomIndex = Math.floor(Math.random() * (index + 1));
+      [number[index], number[randomIndex]] = [number[randomIndex], number[index]]
+    }
+    return number
+  }
+}
+
+// view.displayCars() 
+
+console.log(utility.getRandomNumberArray(10))
